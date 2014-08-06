7a224ace6c8551ffd8af7bb8cd4c64269747da7e
YKG <yekaige2010@gmail.com>
2014-08-06 19:02:17 +0800

    e11 solution with head

--- e11.js
+++ e11.js
@@ -2,7 +2,8 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type': 'text/plain'});
 	var stream = fs.createReadStream(process.argv[3]);
 	stream.pipe(response);
 });
-server.listen(process.argv[2]);
+server.listen(+process.argv[2]);
