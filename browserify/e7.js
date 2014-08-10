c0e1a465ba3cad0a0aa103c39f674586bafe859f
YKG <yekaige2010@gmail.com>
2014-08-10 16:01:00 +0800

    e7 offical solution

--- browserify/e7.js
+++ browserify/e7.js
@@ -3,12 +3,13 @@ var _ = require("lodash");
 var sprintf = require("sprintf");
 var src = fs.readFileSync("/usr/local/lib/node_modules/browserify-adventure/problems/using_transforms/wake.txt", 'utf8');
 var lines = src.split('\n');
-//console.log(lines.length);
+//console.log(lines);
 
 lines.pop();
 
-var i = 0;
-_.map(lines, function(s){
+//var i = 0;
+//_.map(lines, function(s){
+lines.forEach(function(s, i){
 	if(i % 5 === 0)	
 		console.log(sprintf('%3d %s', i, s));
 	else
