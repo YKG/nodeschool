a9a37d52df13cf85c721b7e7e899fffb34ea8382
YKG <yekaige2010@gmail.com>
2014-08-10 21:27:45 +0800

    e7 without confuse

--- stream-adventure/e7.js
+++ stream-adventure/e7.js
@@ -2,17 +2,18 @@ var http = require('http');
 var through = require('through');
 
 http.createServer(function(req, res){
-//	console.log('M: ' , req.method);
-	if(req.method !== 'POST'){
-		return res.end('not post');
+	console.log('M: ' , req.method, req.url);
+	if(req.method === 'POST'){
 //	if(req.method === 'POST'){
 //		req.pipe(through(write, end)).pipe(res);
 //		res.end();
-//		req.pipe(through(write, end)).pipe(res);
+		req.pipe(through(write, end)).pipe(res);
 //		req.pipe(through(write, end)).pipe(process.stdout);
 //		res.end('OK..');
 	}
-	req.pipe(through(write, end)).pipe(res);
+	else
+		return res.end('not post');
+	//req.pipe(through(write, end)).pipe(res);
 }).listen(+process.argv[2]);
 
 function write(buf){
