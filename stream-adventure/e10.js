d23ec9c7222e797ee858bb5c7a254a13b747e23c
YKG <yekaige2010@gmail.com>
2014-08-11 08:18:47 +0800

    e10 offical solution. through has an default end function, it can be omitted.

--- stream-adventure/e10.js
+++ stream-adventure/e10.js
@@ -2,12 +2,11 @@ var trumpet = require('trumpet');
 var through = require('through');
 
 var tr = trumpet();
-process.stdin.pipe(tr).pipe(process.stdout);
 
 var stream = tr.select('.loud').createStream();
 stream.pipe(through(function(buf){
 	this.queue(buf.toString().toUpperCase());	
-}), function(){
-	this.queue(null);
-}).pipe(stream);
+})).pipe(stream);
+
+process.stdin.pipe(tr).pipe(process.stdout);
 
